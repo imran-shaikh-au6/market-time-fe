11fc236387c23b9103caaf0180213ed38fa8acaf
imran-shaikh-au6 <imranshaikh110120@gmail.com>
2020-08-24 12:23:38 +0530

    new m

--- src/Redux/actions/productAction.js
+++ src/Redux/actions/productAction.js
@@ -1,28 +1,33 @@
-import { GetAllProduct, particularProduct,particularProductOwner } from "./productType";
+import {
+    GetAllProduct,
+    particularProduct,
+    particularProductOwner,
+} from "./productType";
 import axios from "axios";
 export const getProducts = () => async (dispatch) => {
-    const res = await axios("https://market-time.herokuapp.com/allProduct/1");
-   
+    const res = await axios(
+        "https://market-time-be.herokuapp.com/allProduct/1"
+    );
+
     dispatch({ type: GetAllProduct, payload: res.data });
 };
 export const particularProductDetail = (id) => async (dispatch) => {
     const res = await axios.get(
-        `https://market-time.herokuapp.com/singleProduct/${id}`
+        `https://market-time-be.herokuapp.com/singleProduct/${id}`
     );
-   
-    const userId= res.data.data.user
+
+    const userId = res.data.data.user;
     const particularProductOwners = await axios.get(
         `https://market-time-be.herokuapp.com/userProfile/${userId}`
     );
-    
+
     dispatch({ type: particularProduct, payload: res.data.data });
     dispatch({
         type: particularProductOwner,
         payload: particularProductOwners.data,
     });
-
 };
-export const addPro = (token,data)=> async (dispatch)=>{
-    console.log(data)
-    console.log(token)
-}
+export const addPro = (token, data) => async (dispatch) => {
+    console.log(data);
+    console.log(token);
+};
